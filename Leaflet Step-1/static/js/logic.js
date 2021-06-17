7c3579b655d72e48ac68f7cd8b09d74516c3eade
calvin c <calvincrouch365@gmail.com>
2021-06-17 01:16:39 -0500

    updated index.html and logic.js

--- Leaflet Step-1/static/js/logic.js
+++ Leaflet Step-1/static/js/logic.js
@@ -6,27 +6,24 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_mo
 
 // perform GET request to queryUrl
 d3.json(queryUrl).then(function (data) {
-    // send data.features object to createFeatures function
     createFeatures(data.features);
 });
 
 function createFeatures(earthquakeData) {
 
-    // function to run for each feature in features array
-    // give each feature a popup
     function onEachFeature(feature, layer) {
         layer.bindPopup("<h3>" + feature.properties.place + "</h3><p>" + feature.properties.mag + " magnitude</p><p>" + new Date(feature.properties.time) + "</p>");
     }
 
-    // set color range for markers
+    // set colorfor markers
     function getColor(d) {
-        return d < 10 ? "#00FF00" : //lime green
-            d >= 10 ? "#9acd32" : //yellow green
-                d >= 30 ? "#FFFF00" : //yellow
-                    d >= 50 ? "f8d568" : //orange yellow
-                        d >= 70 ? "FFA500" : //orange
-                            d >= 90 ? "#FF0000" : //red
-                                "DC143C"; //crimson
+        return d < 10 ? "#00FF00" : 
+            d >= 10 ? "#9acd32" : 
+                d >= 30 ? "#FFFF00" : 
+                    d >= 50 ? "f8d568" : 
+                        d >= 70 ? "FFA500" : 
+                            d >= 90 ? "#FF0000" :
+                                "DC143C"; 
     }
 
     // create geojson layer containing features array
@@ -45,13 +42,13 @@ function createFeatures(earthquakeData) {
         onEachFeature: onEachFeature
     });
 
-    // send earthquakes layer to createMap function
+
     createMap(earthquakes);
 }
 
 function createMap(earthquakes) {
 
-    // define map layer(s)
+    // define map layers
     var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
@@ -59,12 +56,10 @@ function createMap(earthquakes) {
         accessToken: API_KEY
     });
 
-    // define baseMaps object to hold base layers
     var baseMaps = {
         "Light Map": lightmap
     };
 
-    // create overlap object to hold overlay earthquake layer
     var overlayMaps = {
         Earthquakes: earthquakes
     };
